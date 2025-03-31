096547d341c441d8994de348779ac4d49abea48e
Moksh Tehlan <tehlanmoksh@gmail.com>
2025-03-31 23:11:42 +0530

    Refactor environment variable handling and update Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM openjdk:17-jdk-slim
 WORKDIR /
-COPY .prod_env /.prod_env
+COPY prod.env /.prod_env
 COPY /build/libs/*SNAPSHOT.jar /app.jar
 EXPOSE 8080
 CMD ["java","-Dspring.profiles.active=prod", "-jar", "/app.jar"]
--- src/main/java/com/moksh/imposterai/ImposterAiApplication.java
+++ src/main/java/com/moksh/imposterai/ImposterAiApplication.java
@@ -1,18 +1,11 @@
 package com.moksh.imposterai;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ImposterAiApplication {
     public static void main(String[] args) {
-        String profile = System.getProperty("spring.profiles.active", "dev");
-        Dotenv dotenv = Dotenv.configure()
-                .filename("." + profile + "_env")
-                .load();
-
-        dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
         SpringApplication.run(ImposterAiApplication.class, args);
     }
 }
